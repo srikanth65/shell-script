d0d2fe616ec4c23a2206ef0d299cfbfa4aeea30f
srikanth65 <srikanthch0465@gmail.com>
2023-12-21 21:47:19 -0800

    arrays

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-FRUITS=("apples" "oranges" "manoges")
-echo "first fruit in arrary is ${FRUITS[0]}"
+FRUITS=("apple" "orange" "manoge" "strawberry" "grapes")
+echo "first fruit in arrary is: ${FRUITS[0]}"
+echo "second fruit in array is: ${FRUITS[1]}"
+echo "all fruits in array are: $FRUITS[@]"
