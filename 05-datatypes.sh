efbb5a0ac1b8dda1c7a37af322f49f9cc9550fdc
srikanth65 <srikanthch0465@gmail.com>
2023-12-21 21:32:03 -0800

    04-scripts-datatypes

---
+++ 05-datatypes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Type first value to be added:"
+read FIRSTVALUE
+echo "Enter second value to be added:"
+read SECONDVALUE
+echo "Total is $($FIRSTVALUE + $SECONDVALUE)"
+
+echo "Total arguments passed $@"
+echo "How many arguments passed: $#"
+echo "Script name: $0"
