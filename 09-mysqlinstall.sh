451d37552d133b772bcdd7e8cd764e3ed4b2a3cc
srikanth65 <srikanthch0465@gmail.com>
2023-12-22 14:17:22 -0800

    mysqlinstall

--- 09-mysqlinstall.sh
+++ 09-mysqlinstall.sh
@@ -3,12 +3,14 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "Please run script with sudo access"
+    exit 1
 fi
 
 yum list mysql 
 if [ $? -eq 0 ]
 then
     echo "Already installed mysql"
+    exit 1
 else 
     yum install mysql -y
 if [ $? -ne 0 ]
