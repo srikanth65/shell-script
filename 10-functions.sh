64dc8c9a0c4e6fb4e375256c2e9a5a1a41b75345
srikanth65 <srikanthch0465@gmail.com>
2023-12-22 14:40:55 -0800

    mysqlinstall

---
+++ 10-functions.sh
@@ -0,0 +1,26 @@
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+echo "Run the script with sudo access"
+fi
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+    echo "$1 is failed to install"
+    exit 1
+    else
+    echo "$1 is successfully installed"
+    fi 
+}
+CHECK(){
+    if [ $? -eq 0 ]
+    then
+    echo "$1 is already installed"
+    exit 1
+    fi 
+}
+
+yum list mysql 
+CHECK
+yum install mysql -y
+VALIDATE $? "installing mysql"
