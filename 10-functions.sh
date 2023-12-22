8ae6811e5f3a6d730180bc884f97d9cdee625aa8
srikanth65 <srikanthch0465@gmail.com>
2023-12-22 14:48:52 -0800

    mysqlinstall

--- 10-functions.sh
+++ 10-functions.sh
@@ -8,10 +8,10 @@ fi
 VALIDATE(){
     if [ $? -ne 0 ]
     then
-    echo "$1 is failed to install"
+    echo "$2 is failed to install"
     exit 1
     else
-    echo "$1 is successfully installed"
+    echo "$2 is successfully installed"
     fi 
 }
 CHECK(){
