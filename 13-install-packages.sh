78944dcad845dcfdfb7672c125fc347148112b2c
srikanth65 <srikanthch0465@gmail.com>
2023-12-23 10:43:04 -0800

    installpackages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -27,13 +27,13 @@ VALIDATE(){
 
 for package in $@
 do
-yum list $package &>> $LOGFILE
-if [ $? -ne 0 ]
-then
-yum install $package -y &>> $LOGFILE
-VALIDATE $package
-else 
-echo "$Y $package already exists $N" &>> $LOGFILE
-fi
+    yum list $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+    yum install $package -y &>> $LOGFILE
+    VALIDATE $package
+    else 
+    echo "$Y $package already exists $N" &>> $LOGFILE
+    fi
 #yum install $package -y
 done
