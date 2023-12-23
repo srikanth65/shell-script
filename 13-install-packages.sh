703a054171c0c9ad29c3ed70c9cc50d4af36649f
srikanth65 <srikanthch0465@gmail.com>
2023-12-23 08:19:07 -0800

    installpackages

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -7,8 +7,18 @@ else
 echo "script executes"
 fi
 
+VALIDATE(){
+    if [ $? -eq 0]
+    then
+    echo "$1 Command executed"
+    else
+    echo "$1 Command execution failed"
+}
+
+
 for package in $@
 do
 echo "entered value $package"
+VALIDATE
 #yum install $package -y
 done
