3e5743354dbc314ccead478181db2b3eb09f1a9b
srikanth65 <srikanthch0465@gmail.com>
2023-12-24 22:08:10 -0800

    delte_old_logs

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -4,10 +4,14 @@ SOURCE_DIR=/home/centos/test
 
 if [ ! -d $SOURCE_DIR ]
 then
-    echo -e "$SOURCE_DIR source directory does not exists"
-fi 
+    echo -e "$SOURCE_DIR is doesn't exist"
+fi
 
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -name "*.log" -mtime +12)
 
+WHILE IFS= read -r line
+do
+echo "Deleting $line"
+rm -rf $line
 
-SEARCH=$(find /home/centos/test -type f -name "*.log" -mtime +10)
-echo "$SEARCH"
+done <<<$FILES_TO_DELETE
