2f46daaa71c64a5be0ed42cd5d274c35a7369347
srikanth65 <srikanthch0465@gmail.com>
2023-12-24 21:53:09 -0800

    delte_old_logs

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-ID=$(id -u)
-if [ $ID -ne 0 ]
-then
-echo "run script with sudo permissions"
-fi
 
-SEARCH=$(find /home/centos/test -type f -name "*.log" -mtime -15)
+SOURCE_DIR=/home/centos/test1
+
+if [ ! d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR source directory does not exists"
+fi 
+
+
+
+SEARCH=$(find /home/centos/test -type f -name "*.log" -mtime +10)
 echo "$SEARCH"
