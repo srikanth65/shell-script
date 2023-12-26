6f1bc3de1edc85dba55f8d21fe74efeada9071f1
srikanth65 <srikanthch0465@gmail.com>
2023-12-25 16:22:35 -0800

    delte_old_logs

--- 16-ec2-instace.sh
+++ 16-ec2-instace.sh
@@ -3,14 +3,14 @@ AMI="ami-03265a0778a880afb"
 SECURITY="sg-0773d1c611d536265"
 INSTANCE=("mongoDB" "redis" "mysql" "rabbitmq" "catalouge" "cart" "user" "shipping" "payments" "ratings")
 
-for i in ${INSTANCE[@]}
-do
+#for i in ${INSTANCE[@]}
+#do
     #echo "$i"
-    if [ $i == redis ]
-    then 
-      IP=$(aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids sg-0773d1c611d536265 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]' \
+#    if [ $i == redis ]
+#    then 
+aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids sg-0773d1c611d536265 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]' \
         --query 'Instances[0].[PrivateIpAddress]' \ 
-        --output text)
-    fi
+        --output text
+#    fi
     echo "$i:$IP"
-done 
+#done 
