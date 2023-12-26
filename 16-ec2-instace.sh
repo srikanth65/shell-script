107eb49cc4a2d18a630f2501ad4c9d9f4e1addaf
srikanth65 <srikanthch0465@gmail.com>
2023-12-25 16:37:04 -0800

    delte_old_logs

--- 16-ec2-instace.sh
+++ 16-ec2-instace.sh
@@ -6,9 +6,10 @@ INSTANCE=("mongoDB" "redis" "mysql" "rabbitmq" "catalouge" "cart" "user" "shippi
 for i in ${INSTANCE[@]}
 do
 
-    if [ $i == mysql ]
+    if [ $i == rabbitmq ]
     then 
        IP=$(aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids $SECURITY --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].[PrivateIpAddress]' --output text)
+       echo "$i:$IP"
     fi
-    echo "$i:$IP"
+    #echo "$i:$IP"
 done 
