247c4c66150d02f6c2a993d40f54968d268f7bfa
srikanth65 <srikanthch0465@gmail.com>
2024-01-02 13:04:00 -0800

    instance creation

--- 16-ec2-instace.sh
+++ 16-ec2-instace.sh
@@ -10,12 +10,12 @@ do
 
     if [ $i == "rabbitmq" ] || [ $i == "mongoDB" ] || [ $i == "redis" ]
     then 
-       INSTANCE_TYPE="t3.medium"
+       INSTANCE_TYPE="t3.small"
     else 
         INSTANCE_TYPE="t2.micro"
     fi
-   IP=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].[PublicIpAddress]' --output text)
-        echo "ServerName: $i  Public_IP_Address: $IP"
+   IP=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].[PrivateIpAddress]' --output text)
+        echo "ServerName: $i  Private_IP_Address: $IP"
  
 # create R53 record, make sure you delete existing record
 aws route53 change-resource-record-sets \
